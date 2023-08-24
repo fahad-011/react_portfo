e2647e76dca89c6c1e6b6ed6c6a4839114c7b5c3
roc102 <kyroc102@gmail.com>
2023-08-24 10:48:34 +0530

    fix

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -35,7 +35,7 @@ const Home = () => {
           <a href="https://github.com/roc102" className="link dim text-white mr-3">
             <FaGithub className="f2 col" />
           </a>
-          <a href="https://drive.google.com/file/d/13PMvr4hD0PTAC5Ph6IlLaJZwpcfyYBD2/view?usp=sharing" className="link dim text-white mr-3">
+          <a href="https://drive.google.com/file/d/16uag0LBe2taD9dz6kSRlpM095uGL1SLW/view?usp=sharing" className="link dim text-white mr-3">
             <FaFilePdf className="f2 col" />
           </a>
             <a href="mailto:mdfahadrahman1969@gmail.com" className="link dim text-white">
