4a56e264d576a573190f65b61bc619166e03b306
roc102 <mdfahadrahman1969@gmail.com>
2025-01-13 02:23:17 +0530

    updated changes

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -49,7 +49,7 @@ const Home = () => {
             <FaGithub className="f2 col" />
           </a>
           <a
-            href="https://drive.google.com/file/d/1BtLZIA_Z9uZ2OFeVKfrMxlqfNfTAQKI-/view?usp=sharing"
+            href="https://drive.google.com/file/d/1O1cDPqEiYy1d85WDbIIi18sQtKItTjVf/view?usp=sharing"
             className="link dim text-white mr-3"
           >
             <FaFilePdf className="f2 col" />
